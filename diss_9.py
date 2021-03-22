3ae502172b2a8c12726b1979331a27d00e5b75b0
Katie Zhao <kwzhao@umich.edu>
2021-03-22 16:10:20 -0400

    Update diss_9.py

--- diss_9.py
+++ diss_9.py
@@ -6,34 +6,51 @@ import unittest
 # Task 1: Get the URL that links to the Pokemon Charmander's webpage.
 # HINT: You will have to add https://pokemondb.net to the URL retrieved using BeautifulSoup
 def getCharmanderLink(soup):
-    pass
+    anchor = soup.find("div", class_ = "infocard-list infocard-list-pkmn-lg")
+    anchor2 = anchor.find_all("span", class_="infocard-lg-img")[3]
+    print(anchor2)
+    anchor3 = anchor2.find("a")["href"]
+    print(anchor3)
+    return "https://pokemondb.net" + anchor3
+
 
 # Task 2: Get the details from the box below "Egg moves". Get all the move names and store
 #         them into a list. The function should return that list of moves.
 def getEggMoves(pokemon):
     url = 'https://pokemondb.net/pokedex/'+pokemon
-    #add code here
-
+    r = requests.get(url)
+    soup = BeautifulSoup(r.text, "html.parser")
+    anchor = soup.find_all("table", class_ = "data-table")[2]
+    anchor2 = anchor.find_all("tr")
+    moves = []
+    for row in anchor2[1:]:
+        content = row.find("a", class_ = "ent-name").text.strip()
+        moves.append(content)
+    return moves
+    
 # Task 3: Create a regex expression that will find all the times that have these formats: @2pm @5 pm @10am
 # Return a list of these times without the '@' symbol. E.g. ['2pm', '5 pm', '10am']
 def findLetters(sentences):
     # initialize an empty list
-    
+    lst = []
 
     # define the regular expression
-    
+    reg = "\@(\d+\s?[ap]m)"
 
     # loop through each sentence or phrase in sentences
-    
+    for sentence in sentences:
+        sentence = re.findall(reg, sentence)
 
     # find all the words that match the regular expression in each sentence
-       
+        for word in sentence:
+            lst.append(word)
+        
 
     # loop through the found words and add the words to your empty list
 
 
     #return the list of the last letter of all words that begin or end with a capital letter
-
+    return lst
 
 
 def main():
